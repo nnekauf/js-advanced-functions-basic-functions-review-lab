5dafe5596f3602065aa47c7a0dd8ddeec255a5c6
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-30 01:00:41 -0400

    commit tree c446a2556e2e36f68833b89de2180974b86b10d1

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(string="*") { // 2
+    let innerFunction =  function(lang="special") { // 3
+        return `You are ${string}${lang}${string}!` // 4
+    }
+    return innerFunction
+}
+  
+  wrapAdjective()
+  mondayWork()
+  saturdayFun()
